351167b500cbbb662adb863a1fed80d188ab8167
Ivan <ivanecankov@gmail.com>
2024-02-15 15:05:41 +0100

    ball can now be shot manually

--- Gameloop.py
+++ Gameloop.py
@@ -24,6 +24,20 @@ while True:
             pygame.quit()
             sys.exit()
 
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:  # Left mouse button clicked
+            game_field.drag_start = pygame.mouse.get_pos()
+
+        elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:  # Left mouse button released
+            drag_end = pygame.mouse.get_pos()
+            delta_x = drag_end[0] - game_field.drag_start[0]
+            delta_y = drag_end[1] - game_field.drag_start[1]
+
+            # Add the delta values to the ball's velocity
+            game_field.ball.velocity[0] -= delta_x / 5.0  # You can adjust the scale factor for sensitivity
+            game_field.ball.velocity[1] -= delta_y / 5.0
+
+            game_field.drag_start = None  # Reset the drag start position
+
     # Update and draw the game field
     game_field.update(dt)
     game_field.draw(window)
